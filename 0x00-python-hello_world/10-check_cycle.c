72d54bd7d8d52d4ab09e3b5c60295f085f05749a
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-11-30 19:57:59 +0200

    addin files for 10.

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ *
+ * @list: a struct for the linked list
+ *
+ * Description: singly linked list node structure
+ */
+int check_cycle(listint_t *list)
+{
+    
+}
