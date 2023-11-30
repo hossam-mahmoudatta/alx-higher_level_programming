e40506f62ccb3ed6c0de74f844b05e93378de40c
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-11-30 20:07:58 +0200

    das

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,5 +9,21 @@
  */
 int check_cycle(listint_t *list)
 {
-    
+    listint_t *slow = list;
+    listint_t *fast = list;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+        {
+            // Cycle detected
+            return 1;
+        }
+    }
+
+    // No cycle detected
+    return 0;
 }
