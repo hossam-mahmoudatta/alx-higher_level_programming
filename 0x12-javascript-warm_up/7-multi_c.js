2fc0587f7bb261c453f207de2f5d8356e53c3e5d
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-06-19 19:44:09 +0300

    edited task-7 again

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const selectedString = ['C is fun'];
+const selectedString = 'C is fun';
 
 const arg = process.argv[2];
 const intValue = parseInt(arg);
@@ -8,6 +8,6 @@ if (isNaN(intValue)) {
   console.log('Missing number of occurrences');
 } else if (!isNaN(intValue)) {
   for (let i = 0; i < intValue; i++) {
-    console.log(selectedString[i]);
+    console.log(selectedString);
   }
 }
