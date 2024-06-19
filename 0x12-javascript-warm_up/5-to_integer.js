2e6b6ac35729954ea21c8b1f21b1bbef2f220d04
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-06-19 17:30:11 +0300

    edited task-5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const arg  = process.argv[2];
+const intValue = parseInt(arg);
+
+if (isNaN(intValue)) {
+  console.log('Not a number');
+} else if (!isNaN(intValue)) {
+  console.log('My number: ' + intValue);
+}
+//  else { console.log('undefined is undefined'); }
