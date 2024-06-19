9df2a3a0859dad74e7894de00db2a353f469f82a
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-06-19 16:00:57 +0300

    edited task-3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 const argsLength = args.length;
-argsLength < 1 ? 'No argument' : console.log(args);
+if (argsLength < 1) {
+  console.log('No argument');
+} else {
+  console.log(args);
+}
+// argsLength < 1 ? 'No argument' : console.log(args);
 // console.log(args);
