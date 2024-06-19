dc44d13f3cd9f3bf3ab40716cd4b42deca8bd00f
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-06-19 15:20:04 +0300

    edited task-2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,11 @@
 #!/usr/bin/node
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+const args = process.argv.slice(2);
+const argsLength = (
+  args.length < 1
+    ? 'No argument'
+    : args.length === 1
+      ? 'Argument found'
+      : 'Arguments found'
+);
+console.log(argsLength);
+// console.log(args);
