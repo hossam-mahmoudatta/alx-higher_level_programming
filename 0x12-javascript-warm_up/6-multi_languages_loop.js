48a8464cbd819aab13f5414bfda2d8efb29f14f4
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-06-19 19:29:46 +0300

    edited task-6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const arrayOfStrings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < arrayOfStrings.length; i++) {
+  console.log(arrayOfStrings[i]);
+}
