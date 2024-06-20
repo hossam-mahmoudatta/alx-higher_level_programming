d8a296dd4a8b0fece20c546fb81998433fbed8f8
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-06-21 01:34:33 +0300

    edited some comments

--- 0x12-javascript-warm_up/100-let_me_const.js
+++ 0x12-javascript-warm_up/100-let_me_const.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-// I dont really understand this task
+// I don't really understand this task
 
-// Dunno why this code didnt work...
+// Dunno why this code didn't work...
 // const mainJS = require('./100-main.js');
 // mainJS.myVar = 333;
 // module.exports = mainJS;
