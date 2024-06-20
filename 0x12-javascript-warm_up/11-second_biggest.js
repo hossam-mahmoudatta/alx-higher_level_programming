22070eddc6eda49f6301d06e9148cc0f4e53fec4
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-06-20 12:23:53 +0300

    adding task-1

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+function sortingInput (...args) {
+  // args = process.argv.slice(2);
+
+  if (args.length <= 1) {
+    console.log(0);
+    return null;
+  } else {
+    const sortedArray = args.sort((a, b) => b - a);
+    return sortedArray;
+  }
+}
+
+const argus = process.argv.slice(2);
+
+console.log(sortingInput(...argus)[1]);
