2bb64a5df42faf10da2ca54cfc6795b3c9e807cd
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-06-21 01:14:04 +0300

    Trying another method of export in task-13

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -2,6 +2,15 @@
 
 // I dont really understand this task
 
-exports.add = function (a, b) {
+/*
+  The code inside the main.js that will use from this function
+
+  const add = require('./13-add').add;
+  -> require means that it will import the 13-add file
+  -> .add means it will access this function
+
+  console.log(add(3, 5));
+*/
+export function add (a, b) {
   return (a + b);
-};
+}
