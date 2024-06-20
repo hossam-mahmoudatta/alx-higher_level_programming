6459d69bba7128140a5b32e90d612cb3c89b3b23
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-06-21 01:33:14 +0300

    edited task-100

--- 0x12-javascript-warm_up/100-let_me_const.js
+++ 0x12-javascript-warm_up/100-let_me_const.js
@@ -2,8 +2,10 @@
 
 // I dont really understand this task
 
-const mainJS = require('./100-main.js');
+// Dunno why this code didnt work...
+// const mainJS = require('./100-main.js');
+// mainJS.myVar = 333;
+// module.exports = mainJS;
 
-mainJS.myVar = 333;
-
-module.exports = mainJS;
+// What worked was this:
+myVar = 333;
