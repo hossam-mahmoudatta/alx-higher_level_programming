e2fd6b93fbadaaafd99bcc09c4540e63528c4df0
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-06-21 05:40:41 +0300

    edited task-4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,8 +1,36 @@
 #!/usr/bin/node
-if (process.argv[2] && process.argv[3]) {
-  console.log(process.argv[2] + ' is ' + process.argv[3]);
-} else if (process.argv[2]) {
-  console.log(process.argv[2] + ' is undefined');
-} else {
-  console.log('undefined is undefined');
+
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const selectedString = 'X';
+    let output = '';
+
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        output += selectedString;
+      }
+      console.log(output);
+      output = '';
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 }
+
+module.exports = Rectangle;
