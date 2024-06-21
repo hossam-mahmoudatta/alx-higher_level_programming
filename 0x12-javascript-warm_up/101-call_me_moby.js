f29b64386c7490c890aef03cecd71526da03fa94
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-06-21 03:01:33 +0300

    edited task-101

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 
 // Don't understand this code either
+
 module.exports = {
-  CallMeMoby (x, theFunction) {
+  callMeMoby (x, theFunction) {
     for (let i = 0; i < x; i++) {
       theFunction();
     }
