113009aecb3e0ba3da4b724ec1c3a236ade15c26
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-06-23 17:07:45 +0300

    editing task-7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
-const selectedString = 'C is fun';
 
-const arg = process.argv[2];
-const intValue = parseInt(arg);
+module.exports = {
+  nbOccurences (list, searchElement) {
+    let occurrences = 0;
 
-if (isNaN(intValue)) {
-  console.log('Missing number of occurrences');
-} else if (!isNaN(intValue)) {
-  for (let i = 0; i < intValue; i++) {
-    console.log(selectedString);
+    for (let i = 0; i < list.length; i++) {
+      if (list[i] === searchElement) {
+        occurrences++;
+      }
+    }
+    return (occurrences);
   }
-}
+};
