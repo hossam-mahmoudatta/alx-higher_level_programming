05e066008620a0518e1436ef5bf866deec3e6956
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-06-23 23:25:30 +0300

    editing task-100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-const list = require('./100-data');
+const list = require('./100-data').list;
 
 // Pass a function to map
 const map1 = list.map((value, index) => value * index);
 
 console.log(list);
 console.log(map1);
-// Expected output: Array [2, 8, 18, 32]
