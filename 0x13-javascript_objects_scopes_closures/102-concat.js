de0ed10c551aa8e55b152ec0007774686fca958e
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-06-24 04:23:44 +0300

    tweaking the code

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,9 +2,8 @@
 
 // We'll use the fs functions, that are related to files
 const fileSystem = require('fs');
-let combinedString = '';
 
-combinedString = combinedString.concat(fileSystem.readFileSync(process.argv[2]));
-combinedString = combinedString.concat(fileSystem.readFileSync(process.argv[3]));
+const firstArgument = fileSystem.readFileSync(process.argv[2]).toString();
+const secondArgument = fileSystem.readFileSync(process.argv[3]).toString();
 
-fileSystem.writeFileSync(process.argv[4], combinedString);
+fileSystem.writeFileSync(process.argv[4], firstArgument + secondArgument);
