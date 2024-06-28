8bb293ac487cd677d47c35cc11f1cb1b04c63bf3
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-06-28 04:53:05 +0300

    removed excess line

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,8 +4,6 @@ number = random.randint(-10000, 10000)
 # YOUR CODE HERE
 last_digit = abs(number) % 10  # Using abs() to handle negative numbers
 
-print(f"The last digit of {number} is {last_digit}")
-
 if (last_digit > 5):
     print("Last digit of {0} is {1} and is greater than 5".format(number, last_digit))
 elif (last_digit == 0):
